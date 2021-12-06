8a56c7a56b9f5e0c54a34666e5f6b7ce78371f0e
Pikapica19 <shirsat.priyanka06@gmail.com>
2021-12-07 00:04:26 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmiResult, setBmiResult] = useState(null);
+  const [status, setStatus] = useState("");
+
+  function calculateBMI() {
+    let bmi = Number(weight / (height / 100) ** 2).toFixed(2);
+    setBmiResult(bmi);
+
+    let bmiStatus = getStatus(bmi);
+    setStatus(bmiStatus);
+
+    setHeight("");
+    setWeight("");
+  }
+  function getStatus(bmi) {
+    if (bmi < 18.5) return "Underweight 😐";
+    else if (bmi >= 18.5 && bmi < 24.9) return "Normal 😎";
+    else if (bmi >= 25 && bmi < 29.9) return "Overweight 😵";
+    else return "Obese";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fullview">
+    <div className="box">
+      <form className="form">
+        <h1 className="header"> BMI Calculator</h1>
+        <div className="height">
+          <label>
+            Height:
+          </label>
+          <br/>
+          <input
+            className="h"
+            type="text"
+            placeholder="Height in cm"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <div className="weight">
+          <label>
+            Weight:
+          </label>
+          <br/>
+          <input
+            className="w"
+            type="text"
+            placeholder="Weight in kg"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div className="button">
+          <button
+            className="button1"
+            type="Button"
+            onClick={calculateBMI}
+          >
+            Calculate BMI
+          </button>
+        </div>
+        {bmiResult && (
+          <div className="bmi">
+            <p>Your BMI is: {bmiResult} </p>
+            <p>You are currently: {status}</p>
+          </div>
+        )}
+      </form>
     </div>
+    <div className="img1">
+    <img src="./images/girl.png" height = {400}/>
+    </div>
+    <div className="img2">
+      <img src="./images/boy.png" height = {400}/>
+    </div>
+    </div>    
   );
 }
-
-export default App;
